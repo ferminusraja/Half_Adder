ff5703916bb34ea88433e6c5976035e3b0e95a99
Dr.A.Ferminus Raj <ferminusraj83@gmail.com>
2025-10-15 16:06:10 +0530

    Add files via upload

---
+++ H_Adder.v
@@ -0,0 +1,12 @@
+module H_Adder (
+    input  wire a, b,     // Inputs
+    output wire sum,      // Sum output
+    output wire carry     // Carry output
+);
+
+    // Logic equations
+    assign sum   = a ^ b;   // XOR for sum
+    assign carry = a & b;   // AND for carry
+
+endmodule
+
